234dd129d8816056029b58a2e9b9523dd9bd2a4e
Moosa <Moosa>
2022-03-21 12:37:39 +0500

    fcuk this

--- get_saved.py
+++ get_saved.py
@@ -25,45 +25,45 @@ def liked(access_code):
     }
 
     r = requests.post(token_url,data=data,headers=header)
-    # print(r.text)
-    repsonse1 = r.json()
+    print(r.text)
+    # repsonse1 = r.json()
     
-    token1 = repsonse1["access_token"]
-    token2 = repsonse1["refresh_token"]
-    # S = []
-    # print(r.text)
-    sauce = "https://api.spotify.com/v1/me/tracks?offset=0&limit=50"
+    # token1 = repsonse1["access_token"]
+    # token2 = repsonse1["refresh_token"]
+    # # S = []
+    # # print(r.text)
+    # sauce = "https://api.spotify.com/v1/me/tracks?offset=0&limit=50"
 
-    a = token1
-    header = {
-        "content_type": "appliaction/json",
-        "Authorization": f"Bearer {a}",
+    # a = token1
+    # header = {
+    #     "content_type": "appliaction/json",
+    #     "Authorization": f"Bearer {a}",
         
-    }
+    # }
 
 
-    r = requests.get(sauce,headers=header)
-    S = []
+    # r = requests.get(sauce,headers=header)
+    # S = []
     
-    stuff = r.json()
-    # print(stuff["next"])
-    for k in range(0,len(stuff["items"])):
-        S.append(stuff["items"][k]["track"]["name"])
+    # stuff = r.json()
+    # # print(stuff["next"])
+    # for k in range(0,len(stuff["items"])):
+    #     S.append(stuff["items"][k]["track"]["name"])
 
-    a = []
+    # a = []
 
-    while stuff["next"]!=None:
-        sauce = stuff["next"]
-        r = requests.get(sauce,headers=header)
-        stuff = r.json()
-        O = stuff["items"]
-        for k in range(0,len(O)):
-            artist = O[k]["track"]["artists"][0]["name"]
-            song = stuff["items"][k]["track"]["name"]
-            P = song + artist
-            S.append(P)
+    # while stuff["next"]!=None:
+    #     sauce = stuff["next"]
+    #     r = requests.get(sauce,headers=header)
+    #     stuff = r.json()
+    #     O = stuff["items"]
+    #     for k in range(0,len(O)):
+    #         artist = O[k]["track"]["artists"][0]["name"]
+    #         song = stuff["items"][k]["track"]["name"]
+    #         P = song + artist
+    #         S.append(P)
 
 
-    return S
+    return "S"
 
 # print(liked("AQAPLUAOn3tbMU7_ASlSAWwwdr6dsb0oOR2p8i7iubgYNw_fNT_LcXi2GwEl3KoP-8c8fXhz5hQhVUwC7L69LnfK6AugJCi26LxrBExdXw_-OxlmrSWquQZPVTGD9dr8EjLh9qp7S5XBG0IQNpzkpm2TNdBKaHktLKl8ossD7VeZicxnYa-FLos4Ldk"))
